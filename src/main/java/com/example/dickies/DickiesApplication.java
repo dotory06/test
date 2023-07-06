2342017db3aced593b73940e70c847408168b23c
“김승현” <skim3414@energyx.co.kr>
2023-07-06 17:30:03 +0900

    test project

---
+++ src/main/java/com/example/dickies/DickiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dickies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DickiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DickiesApplication.class, args);
+	}
+
+}
